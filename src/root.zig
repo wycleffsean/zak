95226f1933e2c341527c2f002de1942950314ad5
Sean Carey <wycleffsean@gmail.com>
2025-06-18 21:51:44 -0400

    describe sessions
    communicate with kak via "kak -p" and mkfifo

--- src/root.zig
+++ src/root.zig
@@ -1,14 +1,31 @@
 const std = @import("std");
 
+// const mkfifo = @extern(fn (path: [*:0]const u8, mode: u32) callconv(.C) c_int);
+
+const c = @cImport({
+    @cInclude("sys/types.h");
+    @cInclude("sys/stat.h");
+});
+
 const KakSession = struct {
     name: []const u8,
+    cwd: []const u8,
+    buffers: [][]const u8,
+    clients: [][]const u8,
+
+    const Self = @This();
+
+    fn formatForFzf(self: *const Self, allocator: std.mem.Allocator) ![]const u8 {
+        const project = std.fs.path.basename(self.cwd);
+        return std.fmt.allocPrint(allocator, "{s} · name[{s}] · buffers[{d}] · clients[{d}]", .{ project, self.name, self.buffers.len, self.clients.len });
+    }
 };
 
 pub fn selectSession(allocator: std.mem.Allocator, sessions: []KakSession) !?KakSession {
     var fzf_input = std.ArrayList([]const u8).init(allocator);
     var mapping = std.StringHashMap(KakSession).init(allocator);
     for (sessions) |session| {
-        const line = try std.fmt.allocPrint(allocator, "Session: {s}", .{session.name});
+        const line = try session.formatForFzf(allocator);
         try fzf_input.append(line);
         try mapping.put(line, session);
     }
@@ -72,8 +89,7 @@ pub fn kakSessions(allocator: std.mem.Allocator) ![]KakSession {
         const session_name = try allocator.dupe(u8, entry.name);
         const session_path = try std.fs.path.join(allocator, &.{ sessions_path, session_name });
         defer allocator.free(session_path);
-        // const session_info = try describeSession(allocator, session_path);
-        if (isSocketAlive(session_path)) try sessions.append(.{ .name = session_name });
+        if (isSocketAlive(session_path)) try sessions.append(try describeSession(allocator, session_name));
     }
 
     return sessions.toOwnedSlice();
@@ -85,46 +101,60 @@ fn isSocketAlive(path: []const u8) bool {
     return true;
 }
 
-fn describeSession(allocator: std.mem.Allocator, session_name: []const u8) ![]const u8 {
-    // const kak_cmd = try std.mem.concat(allocator, u8, &.{ "nop %sh{ echo Kakoune session: ", session_name, " } " });
-    // defer allocator.free(kak_cmd);
-    const kak_cmd =
-        \\ {
-        \\     echo
-        \\     printf "Session: %s\n" "${kak_session}"
-        \\     printf "Current working directory: %s\n" "${PWD}"
-        \\     eval set -- "${kak_buflist}"
-        \\     printf "Buffers (%d):\n" $#
-        \\     for bufname in "$@"; do
-        \\         printf "\t%s\n" "${bufname}"
-        \\     done
-        \\     eval set -- "${kak_client_list}"
-        \\     printf "Clients (%d):\n" $#
-        \\     for clientname in "$@"; do
-        \\         printf "\t%s\n" "${clientname}"
-        \\     done
-        \\ }
-    ;
+fn describeSession(allocator: std.mem.Allocator, session_name: []const u8) !KakSession {
+    const fifo_path = "/tmp/zak_session_info_fifo";
+    _ = try std.process.Child.run(.{
+        .allocator = allocator,
+        .argv = &[_][]const u8{ "mkfifo", fifo_path },
+    });
+    defer std.fs.deleteFileAbsolute(fifo_path) catch {};
+
+    const kak_script = try std.fmt.allocPrint(allocator,
+        \\nop %sh{{
+        \\  {{
+        \\    printf '{{'
+        \\    printf '"name":"%s",' "$kak_session"
+        \\    printf '"cwd":"%s",' "$PWD"
+        \\
+        \\    eval set -- "$kak_buflist"
+        \\    printf '"buffers":['
+        \\    first=1
+        \\    for buf in "$@"; do
+        \\      [ "$first" -eq 1 ] && first=0 || printf ','
+        \\      printf '"%s"' "$buf"
+        \\    done
+        \\    printf '],'
+        \\
+        \\    eval set -- "$kak_client_list"
+        \\    printf '"clients":['
+        \\    first=1
+        \\    for client in "$@"; do
+        \\      [ "$first" -eq 1 ] && first=0 || printf ','
+        \\      printf '"%s"' "$client"
+        \\    done
+        \\    printf ']}}'
+        \\  }} > {s}
+        \\}}
+    , .{fifo_path});
 
     var process = std.process.Child.init(&[_][]const u8{ "kak", "-p", session_name }, allocator);
     process.stdin_behavior = .Pipe;
-    process.stdout_behavior = .Pipe;
-    process.stderr_behavior = .Pipe;
     try process.spawn();
+    const stdin = process.stdin.?;
 
-    // defer allocator.free(process.stdout);
-    // defer allocator.destroy(process.stderr);
+    _ = try stdin.write(kak_script);
+    stdin.close();
+    process.stdin = null; // we closed it ourselves; avoid a panic
 
-    if (process.stdin) |stdin| {
-        std.debug.print("writing in\n", .{});
-        _ = try stdin.write(kak_cmd);
-        stdin.close();
-    }
+    const fifo_file = try std.fs.openFileAbsolute(fifo_path, .{});
+    defer fifo_file.close();
+    const json = try fifo_file.readToEndAlloc(allocator, 1024);
 
-    if (process.stdout) |stdout| {
-        std.debug.print("writing out\n", .{});
-        return try stdout.readToEndAlloc(allocator, 1024);
-    }
+    // std.debug.print("result: '{s}'\n", .{json});
+    const session = try std.json.parseFromSlice(KakSession, allocator, json, .{});
+    defer session.deinit();
 
-    return "no result";
+    _ = try process.wait();
+
+    return session.value;
 }
